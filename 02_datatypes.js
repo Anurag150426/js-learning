e6481e8b8bfe16871c3bd893d0a8004513c24e85
Anurag150426 <kumaranu8975@gmail.com>
2025-04-17 23:18:55 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as new version
+
+//alert(3+3)// we are not allowed to use alert in node js, it is used in browser
+//console.log(3+3);//code is more readable if we use semicolons and new lines
+//console.log(3+3) // we can also write like this
+
+
+let name="Anurag"//strings
+let age=21
+let isLoggedIn=true
+// number =>2 to pwr 53,infinity,NaN
+//bigint
+//string=>""
+//boolean=>true/false
+//undefined=>no value
+//null=>stand alone value which means nothing
+//symbol=>unique value
+
+//typeof operator is used to find the data type of a variable
+console.log(typeof age);//number
+console.log(typeof name);//string
+console.log(typeof isLoggedIn);//boolean
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
