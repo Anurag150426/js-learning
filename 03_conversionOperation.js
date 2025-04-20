a4f60a7c3a3460e15d8e24d6e3171e9d9abacf3a
Anurag Kumar <kumaranu8975@gmail.com>
2025-04-20 22:01:38 +0530

    Add files via upload

---
+++ 03_conversionOperation.js
@@ -0,0 +1,70 @@
+//let score = 33// gives number
+//let score = "33abc"// will give NAN
+//let score = "null"//0 dega valueIn number
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//conversion in number//
+// "33"=> 33
+//"33abc"=>NaN
+//"true=>1",false=>0
+//"null"=>0
+
+let isLoggedIn= 1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//1 => true
+//0 => false
+//null=>false
+//undefined=>false
+//""=>false
+//Anurag=>true
+
+let someNumber= 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//**********************************Operations******************************************** *//
+
+let value=10
+let negValue=-value
+//console.log(negValue);
+
+//console.log(10+20);
+//console.log(10-20);
+//console.log(10*20);
+//console.log(10**2);//10^2
+//console.log(10/20); 
+
+//let str1="Anurag"
+//let str2="Kumar"
+//let str3 =str1+" "+str2
+//console.log(str3);
+
+//console.log("10"+20);//1020
+//console.log(10+20+"10");//3010 because the string is at the end so first it will add the numbers and then it will concatenate the string
+//console.log("10"+20+10);//102010 
+//console.log(10+"20");
+
+console.log(+true);//1
+console.log(+"");//0
+
+let num1,num2,num3
+
+num1=num2=num3=2+2//It is not prefered due to the complexity of the code
+
+
+
+
+
+
+
