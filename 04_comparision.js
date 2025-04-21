4132b1dab780f5b072d11767bdd8da6bc528373b
Anurag Kumar <kumaranu8975@gmail.com>
2025-04-22 00:06:49 +0530

    Add files via upload

---
+++ 04_comparision.js
@@ -0,0 +1,32 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+
+//console.log("2">1);
+//console.log("02">1);
+
+
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(null==0);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+//all this comparison will give false because null and undefined are not comparable with any number so are avoided in comparison
+
+//===
+console.log(2==="2");//false because it is comparing the type as well and datatype is different
+
+
+
+
+
+
+
+
