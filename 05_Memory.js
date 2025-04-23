1cee8a07c173fde758dd7db321e5e421be6cc82f
Anurag Kumar <kumaranu8975@gmail.com>
2025-04-24 01:03:40 +0530

    Add files via upload

---
+++ 05_Memory.js
@@ -0,0 +1,18 @@
+// Stack(Primitive),Heap(Non Primitive)
+//
+
+let myJersyname = "Anurag";
+
+let anotherName = myJersyname;
+anotherName = "Anurag Kumar";
+console.log(myJersyname);
+console.log(anotherName);
+
+
+let userOne= {
+    email: "userName@gmail.com",
+    upi: "userName@upi"
+}
+
+
+let userTwo = userOne;
